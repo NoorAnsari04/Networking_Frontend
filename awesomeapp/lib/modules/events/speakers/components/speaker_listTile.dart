7f099f0812610bf35649b84b89c2ddc8b3c0784b
Muhammad Noor Ansari <muhammad.noor@reactree.com>
2025-07-08 11:18:00 +0500

    added initials if there is no image, speaker company being shown if not industry person then student

--- awesomeapp/lib/modules/events/speakers/components/speaker_listTile.dart
+++ awesomeapp/lib/modules/events/speakers/components/speaker_listTile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_test_app_flavors/core/constants/color_constants.dart';
+import 'package:my_test_app_flavors/core/shared/user_avatar.dart';
 import 'package:my_test_app_flavors/modules/events/speakers/screens/speaker_profile.dart';
 import 'package:my_test_app_flavors/modules/auth/services/app_user.dart';
 import 'package:my_test_app_flavors/modules/events/speakers/services/speaker_provider.dart';
@@ -23,11 +24,21 @@ class SpeakerListTile extends StatelessWidget {
     return Card(
       child: ListTile(
         contentPadding: EdgeInsets.all(16.0),
-        leading: CircleAvatar(
-          radius: 25.r,
-          backgroundImage: NetworkImage(speaker.imageUrl?.isNotEmpty == true
-              ? speaker.imageUrl!
-              : 'https://via.placeholder.com/150'),
+        leading: UserProfileAvatar(
+          imageUrl: speaker.imageUrl,
+          name: speaker.name,
+          lastName: speaker.lastName,
+          outerRadius: 26.r,
+          innerRadius: 24.r,
+          // radius: 25.r,
+          // backgroundImage: NetworkImage(speaker.imageUrl?.isNotEmpty == true
+          //     ? speaker.imageUrl!
+          //     : 'https://via.placeholder.com/150'),
+          textStyle: TextStyle(
+            fontSize: 12.sp,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
         title: Text(
           speaker.fullName,
@@ -41,19 +52,24 @@ class SpeakerListTile extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Worked in ${speaker.company ?? ''}',
+              speaker.userType == 'Student'
+                  ? 'Student'
+                  : (speaker.company != null && speaker.company!.isNotEmpty
+                  ? 'Works at ${speaker.company}'
+                  : ''),
               style: TextStyle(
                 fontSize: 14.sp,
               ),
             ),
-            Text(
-              speaker.position ?? '',
-              style: TextStyle(
-                fontSize: 10.sp,
-                fontWeight: FontWeight.bold,
-                color: ColorConstants.company,
-              ),
-            ),
+
+            // Text(
+            //   speaker.position ?? '',
+            //   style: TextStyle(
+            //     fontSize: 10.sp,
+            //     fontWeight: FontWeight.bold,
+            //     color: ColorConstants.company,
+            //   ),
+            // ),
           ],
         ),
         trailing: buildTrailingButton(speaker, speakerProvider),
