fcbe3f856a876b09026c2e23d3b658dbf21141e1
Muhammad Noor Ansari <muhammad.noor@reactree.com>
2025-07-21 11:33:44 +0500

    getting accessToken from hive instead of authProvider, Logs added for debugging

--- awesomeapp/lib/modules/events/services/event_networking.dart
+++ awesomeapp/lib/modules/events/services/event_networking.dart
@@ -1,6 +1,7 @@
 import 'package:my_test_app_flavors/core/serviceLocator.dart';
 import 'package:my_test_app_flavors/modules/auth/services/auth_provider.dart';
 import '../../../core/constants/dio_client.dart';
+import '../../../core/services/hive_services.dart';
 import 'event_model.dart';
 import '../../../core/constants/api_constants.dart';
 import 'package:dio/dio.dart' as dio;
@@ -11,8 +12,9 @@ class EventNetworking {
     print('Requesting events from: $url');
 
     try {
-      final token = await serviceLocator<AuthenticationProvider>().authToken();
-      print("Access Token: $token");
+      // final token = await serviceLocator<AuthenticationProvider>().authToken();
+      final token = await HiveService().getAccessToken();
+      print("Access Token from hive: $token");
       final dioInstance = DioClient.getDioInstance();
       final response = await dioInstance.get(url,
           options: dio.Options(
@@ -40,7 +42,8 @@ class EventNetworking {
 
   static Future<EventModel> fetchEventById(eventId) async {
     final url = ApiConstants.baseUrl + "/api/conference/$eventId";
-    print(url);
+    print("Url: ${url}");
+    print("EventId: ${eventId}");
     try {
       final token = await serviceLocator<AuthenticationProvider>().authToken();
       final dioInstance = DioClient.getDioInstance();
@@ -80,4 +83,4 @@ class EventNetworking {
 
 //     return events;
 //   }
-// }
+// }on
