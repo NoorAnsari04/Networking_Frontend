ed66c18be409d65ad2d3c406be8fbc8f71fb946c
Muhammad Noor Ansari <muhammad.noor@reactree.com>
2025-07-31 13:02:23 +0500

    api integrated of contact us

--- awesomeapp/lib/modules/screens/contact_us.dart
+++ awesomeapp/lib/modules/screens/contact_us.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:my_test_app_flavors/core/constants/api_constants.dart';
 import '../../core/constants/color_constants.dart';
+import '../../core/serviceLocator.dart';
 import '../../core/shared/custom_elevated_button.dart';
+import 'package:dio/dio.dart';
+
+import '../auth/services/auth_provider.dart';
 
 class ContactSupport extends StatelessWidget {
   static const id = 'ContactSupport';
@@ -27,8 +32,8 @@ class ContactSupport extends StatelessWidget {
                     },
                     child: SvgPicture.asset(
                       'assets/backward_arrow_head.svg',
-                      height: 20.h,
-                      width: 12.w,
+                      height: 23.h,
+                      width: 25.w,
                     ),
                   ),
                   SizedBox(
@@ -62,13 +67,13 @@ class ContactSupport extends StatelessWidget {
                 fillColor: Colors.grey[200],
                 border: InputBorder.none,
                 contentPadding:
-                    EdgeInsets.symmetric(vertical: 14.0.h, horizontal: 10.0.w),
+                EdgeInsets.symmetric(vertical: 14.0.h, horizontal: 10.0.w),
               ),
-              onChanged: (text) {
-                if (text.isNotEmpty) {
-                  subjectController.clear();
-                }
-              },
+              // onChanged: (text) {
+              //   if (text.isNotEmpty) {
+              //     subjectController.clear();
+              //   }
+              // },
             ),
             SizedBox(height: 20.h),
             TextField(
@@ -81,20 +86,68 @@ class ContactSupport extends StatelessWidget {
                 fillColor: Colors.grey[200],
                 border: InputBorder.none,
                 contentPadding:
-                    EdgeInsets.symmetric(vertical: 14.0.h, horizontal: 10.0.w),
+                EdgeInsets.symmetric(vertical: 14.0.h, horizontal: 10.0.w),
               ),
-              onChanged: (text) {
-                if (text.isNotEmpty) {
-                  messageController.clear();
-                }
-              },
+              // onChanged: (text) {
+              //   if (text.isNotEmpty) {
+              //     messageController.clear();
+              //   }
+              // },
             ),
             SizedBox(height: 20.h),
             Center(
               child: SizedBox(
                 width: double.infinity,
                 child: CustomElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    final subject = subjectController.text.trim();
+                    final message = messageController.text.trim();
+
+                    if (subject.isEmpty || message.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text("Subject and message are required.")),
+                      );
+                      return;
+                    }
+                    try {
+                      final dio = Dio();
+                      final token = await serviceLocator<
+                          AuthenticationProvider>().authToken();
+                      final url = ApiConstants.baseUrl +
+                          ApiConstants.contactSupport;
+                      final response = await dio.post(
+                          url,
+                          options: Options(
+                              headers: {
+                          "Authorization": "Bearer $token",
+                          }
+                          ),
+                          data: {
+                            'subject': subject,
+                            'message': message
+                          }
+                      );
+                      if (response.statusCode == 200 &&
+                          response.data['success'] == true) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text(
+                              "Support message sent successfully.")),
+                        );
+                        subjectController.clear();
+                        messageController.clear();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(response.data['message'] ??
+                                "Failed to send message")));
+                      }
+                    } catch (e) {
+                      print("Dio error: $e");
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text(
+                              "Something went wrong. Please try again later")));
+                    }
+                  },
                   text: 'Submit',
                   manualAction: true,
                 ),
