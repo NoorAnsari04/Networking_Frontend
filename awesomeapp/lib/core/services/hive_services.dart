3bfd768259068b0ca33c66fc2a33bdf5dabfee4e
Muhammad Noor Ansari <muhammad.noor@reactree.com>
2025-07-31 12:04:52 +0500

    Add user persistence with error handling, Clean up user and token persistence logic

--- awesomeapp/lib/core/services/hive_services.dart
+++ awesomeapp/lib/core/services/hive_services.dart
@@ -16,6 +16,7 @@ class HiveService {
   }
 
   Box<AppUser> get _userBox => Hive.box<AppUser>(userBoxName);
+
   Box<String> get _tokenBox => Hive.box<String>(tokenBoxName);
 
   // void saveUser(AppUser user) {
@@ -23,42 +24,51 @@ class HiveService {
   //   saveAccessToken(user.accessToken ?? '');
   // }
   bool saveUser(AppUser user) {
-  try {
-    _userBox.put(userKey, user);
-    saveAccessToken(user.accessToken ?? '');
-    return true; // Indicate success
-  } catch (e) {
-    print('Error saving user: $e');
-    return false; // Indicate failure
+    try {
+      _userBox.put(userKey, user);
+      final x = _userBox.get(userKey);
+      // saveAccessToken(user.accessToken ?? '');
+      return true; // Indicate success
+    } catch (e) {
+      print('Error saving user: $e');
+      return false; // Indicate failure
+    }
   }
-}
-
 
   AppUser? getUser() {
-    return _userBox.get(userKey);
+    final x = _userBox.get(userKey);
+    return x;
   }
 
-  void deleteUser() {
-    _userBox.delete(userKey);
+  // void deleteUser() {
+  //   _userBox.delete(userKey);
+  // }
+  Future<void> deleteUser() async {
+    final userBox = await Hive.openBox<AppUser>(HiveService.userBoxName);
+    await userBox.delete(HiveService.userKey);
+
+    final tokenBox = await Hive.openBox(HiveService.tokenBoxName);
+    await tokenBox.delete(HiveService.accessTokenKey);
+    await tokenBox.delete(HiveService.refreshTokenKey);
   }
 
-  Future<void> saveAccessToken(String token) async {
-    await _tokenBox.put(accessTokenKey, token);
-  }
+  // Future<void> saveAccessToken(String token) async {
+  //   await _tokenBox.put(accessTokenKey, token);
+  // }
 
-  Future<void> saveRefreshToken(String token) async {
-    await _tokenBox.put(refreshTokenKey, token);
-  }
+  // Future<void> saveRefreshToken(String token) async {
+  //   await _tokenBox.put(refreshTokenKey, token);
+  // }
 
-  String? getAccessToken(){
-    return _tokenBox.get(accessTokenKey);
-  }
+  // String? getAccessToken(){
+  //   return _tokenBox.get(accessTokenKey);
+  // }
+  //
+  // String? getRefreshToken(){
+  //   return _tokenBox.get(refreshTokenKey);
+  // }
 
-  String? getRefreshToken(){
-    return _tokenBox.get(refreshTokenKey);
-  }
-
-  Future<void> deleteTokens()async{
+  Future<void> deleteTokens() async {
     await _tokenBox.delete(accessTokenKey);
     await _tokenBox.delete(refreshTokenKey);
   }
